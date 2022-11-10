163e622c06ba604141a43be49d8865d548eb904c
Hieu Ngo <hieu.ngo12989@gmail.com>
2022-11-09 17:01:41 -0800

    added loaderio

--- server/index.js
+++ server/index.js
@@ -7,6 +7,9 @@ const app = express();
 app.use(express.json());
 app.use(morgan('dev'));
 app.use('/', router);
+app.get('/loaderio-3b362b806361fab92945019b57e2a053.txt', (req, res) => {
+  res.send('loaderio-3b362b806361fab92945019b57e2a053')
+})
 
 const port = 3001;
 app.listen(port, console.log(`Listening on port: ${port}`));
